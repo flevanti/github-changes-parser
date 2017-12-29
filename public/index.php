a119c402df1182cb93f012640f04603176c50a96
flevanti <francesco@levanti.net>
2017-12-29 14:41:20 +0000

    wip

--- public/index.php
+++ public/index.php
@@ -10,7 +10,6 @@ mail("frenke@frenke.it",
   "git payload received",
   json_encode($_POST) . "\n\n\n" .
   "SERVER:\n" . json_encode($_SERVER) . "\n\n\n" .
-  "SERVER_VARS:\n" . json_encode($HTTP_SERVER_VARS) . "\n\n\n" .
-  "RAW INPUT:" . file_get_contents('php://input')
+  "RAW INPUT:\n" . file_get_contents('php://input')
 );
 
