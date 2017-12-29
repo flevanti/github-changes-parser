f46a161f70445ca73c5ee60abd62c6f47e609fad
flevanti <francesco@levanti.net>
2017-12-29 14:24:54 +0000

    wip
    Signed-off-by: flevanti <francesco@levanti.net>

--- webhook_endpoint.php
+++ webhook_endpoint.php
@@ -9,5 +9,9 @@
 include("GithubNotifier.php");
 $obj = new GithubNotifier();
 $conf = file_get_contents("config_example");
-$payload = file_get_contents("payload_example");
-$result = $obj->check($payload, "sha1=45778be36d0926f5dfce2264041dcd2cfee8beec", $conf);
+$payload = file_get_contents("payload_example2");
+// Check if the payload is json or urlencoded.
+if (strpos($payload, 'payload=') === 0) {
+  $payload = substr(urldecode($payload), 8);
+}
+$result = $obj->check($payload, "sha1=79b574edf629987a1922387c0fe2e82f38b0b702", $conf);
